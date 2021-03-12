ced757fb6b9e687f384ae60d12d31da3aab4a6d3
William Quirino Ferreira <wqferreira@gmail.com>
2021-03-12 20:37:18 -0300

    Alteracao pagina PRODUTOS.EJS para ler infos dos tenis de uma matriz

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('produtos', { title: 'Express' });
+
+
+  // DADOS DOS PRODUTOS   //
+
+const cadprod = [
+  {
+      codigo: 1,
+      descricao: "Mizuno Wave Sky 3 Masculino Corrida",
+      marca: "Mizuno",
+      modelo: "Wave Sky 3",
+      preco: 240.00
+  },
+  {
+      codigo: 2,
+      descricao: "Asics Raiden 2 Masculino Caminhada",
+      marca: "Asics",
+      modelo: "Raiden 2",
+      preco: 199.90
+    },
+    {
+      codigo: 3,
+      descricao: "Tênis Fila Interceptor Feminino Academia",
+      marca: "Fila",
+      modelo: "Interceptor",
+      preco: 159.99
+    },
+    {
+      codigo: 4,
+      descricao: "Under Armour Sc 3zero Iv Masculino Basquete",
+      marca: "Under Armour",
+      modelo: "Sc 3zero Iv",
+      preco: 650.00
+    },
+]
+
+  res.render('produtos', {cadprod});
 });
 
 module.exports = router;
---
+++ static-database/cad_produtos.js
@@ -0,0 +1,46 @@
+const cadprod = [
+    {
+        codigo: '1',
+        descricao: "Mizuno Wave Sky 3 Masculino Corrida",
+        marca: "Mizuno",
+        modelo: "Wave Sky 3",
+        cor: "Azul",
+        tamanho: '40',
+        genero: "Masculino",
+        estilo_esporte: "Corrida",
+        preco: '240.00'
+    },
+    {
+        codigo: '2',
+        descricao: "Asics Raiden 2 Masculino Caminhada",
+        marca: "Asics",
+        modelo: "Raiden 2",
+        cor: "Preto/Verde",
+        tamanho: '42',
+        genero: "Masculino",
+        estilo_esporte: "Caminhada",
+        preco: '199.90'
+      },
+      {
+        codigo: '3',
+        descricao: "Tênis Fila Interceptor Feminino Academia",
+        marca: "Fila",
+        modelo: "Interceptor",
+        cor: "Rosa Claro",
+        tamanho: '36',
+        genero: "Feminino",
+        estilo_esporte: "Academia",
+        preco: '159.99'
+      },
+      {
+        codigo: '4',
+        descricao: "Under Armour Sc 3zero Iv Masculino Basquete",
+        marca: "Under Armour",
+        modelo: "Sc 3zero Iv",
+        cor: "Preto",
+        tamanho: '44',
+        genero: "Masculino",
+        estilo_esporte: "Basquete",
+        preco: '650.00'
+      },
+  ]
